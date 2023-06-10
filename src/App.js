e087c6c4c799a550cc558a38b3f548edb1fdce09
Brent Doering <brentiscoding@gmail.com>
2023-06-10 17:12:47 -0600

    Imported Link for client side loading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
 import AboutPage from './pages/AboutPage'
 
 function App() {
@@ -39,7 +40,10 @@ function App() {
 
         <Route path='/about' element={<AboutPage />} />
         </Routes>
+
         </div>
+
+        <AboutIconLink />
         </Router>
     )
 }

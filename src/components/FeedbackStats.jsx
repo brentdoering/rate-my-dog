ff71d56abcf1a7d60633e33d6f790fc345043059
Brent Doering <brentiscoding@gmail.com>
2023-06-10 14:34:33 -0600

    Added PropTypes for FeedbackStats

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 function FeedbackStats({ feedback }) {
     //calculate rating average
     let average = feedback.reduce((acc, cur) => {
@@ -10,6 +12,10 @@ function FeedbackStats({ feedback }) {
     <h4>{feedback.length} Reviews</h4>
     <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.array.isRequired,
 }
 
 export default FeedbackStats

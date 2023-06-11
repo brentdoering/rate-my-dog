eeb8e80e2112e9bff41db3400d8ef1acb917971d
Brent Doering <brentiscoding@gmail.com>
2023-06-10 18:38:43 -0600

    Updated code with useContext to refactor into Provider

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,6 +1,8 @@
-import PropTypes from 'prop-types'
+import {useContext} from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackStats({ feedback }) {
+function FeedbackStats() {
+    const { feedback } = useContext(FeedbackContext)
     //calculate rating average
     let average = feedback.reduce((acc, cur) => {
         return acc + cur.rating
@@ -14,8 +16,5 @@ function FeedbackStats({ feedback }) {
     </div>
 }
 
-FeedbackStats.propTypes = {
-    feedback: PropTypes.array.isRequired,
-}
 
 export default FeedbackStats

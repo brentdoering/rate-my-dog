e24c94c8b2e2bd0b42ffb77557ebd72ab7a971dc
Brent Doering <brentiscoding@gmail.com>
2023-06-10 18:45:14 -0600

    Moved app.js functions into context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import { v4 as uuidv4 } from 'uuid'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { useState } from 'react';
 import Header from './components/Header';
-import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
@@ -12,19 +9,6 @@ import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
 
-    const [feedback, setFeedback] = useState(FeedbackData)
-
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
-    }
-
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to do this?')) {
-        setFeedback(feedback.filter((item) => item.id !== id))
-        }
-    }
-
     return (
         <FeedbackProvider>
         <Router>
@@ -33,9 +17,9 @@ function App() {
         <Routes> 
         <Route exact path="/" element={
             <>
-            <FeedbackForm handleAdd={addFeedback} />
+            <FeedbackForm />
             <FeedbackStats />
-            <FeedbackList handleDelete={deleteFeedback} />
+            <FeedbackList />
             </>
         }>
         </Route>
